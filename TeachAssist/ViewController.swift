ce7ce79484027186cff7152f01cea5701e22a6d5
Ben Tran <bentran@bens-imac.local>
2019-02-01 10:51:25 -0500

    Initial Commit

---
+++ TeachAssist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeachAssist
+//
+//  Created by Ben Tran on 2019-02-01.
+//  Copyright © 2019 Ben Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TeachAssistTests/TeachAssistTests.swift
@@ -0,0 +1,34 @@
+//
+//  TeachAssistTests.swift
+//  TeachAssistTests
+//
+//  Created by Ben Tran on 2019-02-01.
+//  Copyright © 2019 Ben Tran. All rights reserved.
+//
+
+import XCTest
+@testable import TeachAssist
+
+class TeachAssistTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
