25461843e68a7c8c39e1da3e3f402fd98a6f4733
Ben Tran <benjamintran0684@gmail.com>
2019-03-02 21:45:35 -0500

    added decimal precision to uicircularProgressBar and the text centered within it by running pod update

--- TeachAssist/ViewControllers/CourseViewController.swift
+++ TeachAssist/ViewControllers/CourseViewController.swift
@@ -9,20 +9,17 @@ import Foundation
 import UIKit
 import UICircularProgressRing
 
+
 class CourseViewController: UIViewController {
 
     @IBOutlet weak var progressRing: UICircularProgressRing!
     override func viewDidLoad() {
         super.viewDidLoad()
-        progressRing.font = UIFont.boldSystemFont(ofSize: 30.0)
+        progressRing.font = UIFont.boldSystemFont(ofSize: 25.0)
         progressRing.value = 87.6
         
+        progressRing.valueFormatter = UICircularProgressRingFormatter(showFloatingPoint:true, decimalPlaces:1)
         
-        
-        
-        
-
-
         // Do any additional setup after loading the view.
     }
     
