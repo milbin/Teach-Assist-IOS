c2438aaec83e7af7c4a882c809ba226448ba2f33
Ben Tran <benjamintran0684@gmail.com>
2019-03-16 18:14:42 -0400

    removed useless outlet

--- TeachAssist/ViewControllers/MainViewController.swift
+++ TeachAssist/ViewControllers/MainViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    var courseView: Course!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
