b6209223d44be4d59fa9e063b063fabdca9dd292
Ben Tran <bentran@Bens-Mac.local>
2019-05-10 10:18:32 -0700

    fixed bug where app would crash if user disabled notifications

--- Pods/UICircularProgressRing/src/UICircularProgressRing/UICircularRingLayer.swift
+++ Pods/UICircularProgressRing/src/UICircularProgressRing/UICircularRingLayer.swift
@@ -265,9 +265,9 @@ class UICircularRingLayer: CAShapeLayer {
 
         case .bordered(let borderWidth, let borderColor):
             let center: CGPoint = CGPoint(x: bounds.midX, y: bounds.midY)
-            let offSet = max(ring.outerRingWidth, ring.innerRingWidth) / 2
+            var offSet = max(ring.outerRingWidth, ring.innerRingWidth) / 2
                             + ((valueKnobStyle?.size ?? 0) / 4)
-                            + (borderWidth * 2)
+            offSet      += (borderWidth * 2)
             let outerRadius: CGFloat = min(bounds.width, bounds.height) / 2 - offSet
             let borderStartAngle = ring.outerCapStyle == .butt ? ring.startAngle - borderWidth : ring.startAngle
             let borderEndAngle = ring.outerCapStyle == .butt ? ring.endAngle + borderWidth : ring.endAngle
