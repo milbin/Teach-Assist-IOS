bda4b7b8801cac662f4c344e1168f37f98bfc573
SARAH TRAN <sarahtran0814@gmail.com>
2019-11-09 15:29:16 -0500

    took out placeholder mark

--- TeachAssist/ViewControllers/MainViewController.swift
+++ TeachAssist/ViewControllers/MainViewController.swift
@@ -194,8 +194,6 @@ class MainViewController: UIViewController {
             if let mark = (course["mark"] as? CGFloat){
                 print(mark)
                 courseView.ProgressBar.value = mark
-            }else if(i == 1){
-                courseView.ProgressBar.value = 90.5
             }else{
                 courseView.ProgressBar.isHidden = true
                 courseView.NATextView.isHidden = false
